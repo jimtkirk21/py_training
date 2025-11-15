0239b6fa83dcdbd65a13358dec8ce2c9b7215854
Adam Bulenda <adam.bulenda.pm@gmail.com>
2025-11-15 12:23:24 +0100

    Delete guessing_game.py

--- guessing_game.py
+++
@@ -1,8 +0,0 @@
-secret = 35
-guess = int(input ('Guess number: '))
-if guess == secret:
-    print (f'You are right the number is {secret}')
-elif guess < secret:
-    print ('Number you put was too low')
-else:
-    print ('Number you put was too high')
